8d7980baabe26a2963b8b382cf2e3527b70b4629
AfiyaZannuba <afiya.zannubaa@gmail.com>
2023-07-31 17:40:22 +0530

    fixed the icon-section

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const dropDownMenu = document.querySelector(".dropdown_menu");
 toggleBtn.onclick = function(){
     dropDownMenu.classList.toggle("open");
     const isOpen = dropDownMenu.classList.contains("open");
-    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-close'
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
 }
